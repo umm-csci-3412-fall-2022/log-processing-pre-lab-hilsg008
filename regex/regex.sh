c71efc2e516b32e1bef774c554722889c5b30050
Lloyd Hilsgen <hilsg008@tycho.morris.umn.edu>
2022-09-13 12:36:53 -0500

    r2 completed

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,2 +1,3 @@
 awk 'match($0, /([A-Z].+), ([a-z].+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < "r0_input.txt"
 awk 'match($0, /([A-Z].*)([A-Z].+)([\.].*)([" "].*)\./, groups) {print "1. " groups[2] "\n" "2." groups[4] "\n" }' < "r1_input.txt"
+awk 'match($0, /([a-z].*)([" "].*)([" "].*)(([" "].*)([" "].*))/, groups) {print "1." groups[3] "\n" "2." groups[4] "\n" }' < "r2_input.txt"
